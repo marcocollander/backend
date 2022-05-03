48f9a79d174b872eb7c7dbe58d86e0863ecd0db2
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-05-03 06:10:43 +0200

    Update server.js file - added functions sendFourZeroFour, sendFile, serveStatic and server

--- exercises/chat/server.js
+++ exercises/chat/server.js
@@ -1,6 +1,42 @@
-// Wbudowany moduł http dostarcza funkcje dotyczące serwera i klienta.
-var http = require('http');
-// Wbudowany moduł fs dostarcza funkcje przeznaczone do pracy z systemem plików.
-var fs = require('fs');
-// Wbudowany moduł path dostarcza funkcje przeznaczone do pracy ze ścieżkami dostępu systemu plików.
-var path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const sendFourZeroFour = (res) => {
+  res.writeHead(404, { "Content-Type": "text/plan" });
+  res.write("Error 404: plik nie został znaleziony");
+  res.end();
+};
+
+const sendFile = (res, filePath, fileContents) => {
+  res.writeHead(200, { "Content-Type": mime.lookup(path.basename(filePath)) });
+  res.end(fileContents);
+};
+
+const serveStatic = (res, cache, absPath) => {
+  if (cache[absPath]) {
+    sendFile(res, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, (exists) => {
+      if (exists) {
+        fs.readFile(absPath, (err, data) => {
+          if (err) {
+            sendFourZeroFour(res);
+          } else {
+            cache[absPath] = data;
+            sendFile(res, absPath, data);
+          }
+        });
+      } else {
+        sendFourZeroFour(res);
+      }
+    });
+  }
+};
+
+const server = (http.createServer = (req, res) => {
+  let filePath = false;
+  if (req.url === "/") {
+    filePath = "public/index.html";
+  }
+});
