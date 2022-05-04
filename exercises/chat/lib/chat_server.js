8d13301144418706b2d88de95ca5a84e98b57d26
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-05-04 05:12:52 +0200

    Update server.js

--- exercises/chat/lib/chat_server.js
+++ exercises/chat/lib/chat_server.js
@@ -1,22 +1,23 @@
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 let io;
 let guestNumber = 1;
 const nickNames = {};
 const namesUsed = [];
 const currentRoom = {};
 
-exports.listen = server => {
-  io = new Server(server);
-  // io.set('log level', 1);
-  io.sockets.on('connection', socket => {
+exports.listen = (server) => {
+  io = new Server(server, {});
+
+  io.set("log level", 1);
+  io.sockets.on("connection", (socket) => {
     guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
-    joinRoom(socket, 'Lobby');
-    handleMessageBrodcasting(socket, nickNames);
+    joinRoom(socket, "Lobby");
+    handleMessageBroadcasting(socket, nickNames);
     handleNameChangeAttempts(socket, nickNames, namesUsed);
     handleRoomJoining(socket);
 
-    socket.on('rooms', () => {
-      socket.emit('rooms', io.sockets.manager.rooms);
+    socket.on("rooms", () => {
+      socket.emit("rooms", io.sockets.manager.rooms);
     });
 
     handleClientDisconnection(socket, nickNames, namesUsed);
@@ -24,9 +25,9 @@ exports.listen = server => {
 };
 
 function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
-  let name = 'Gość' + guestNumber;
+  let name = "Gość" + guestNumber;
   nickNames[socket.id] = name;
-  socket.emit('nameResult', {
+  socket.emit("nameResult", {
     success: true,
     name: name,
   });
@@ -38,51 +39,51 @@ function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
 function joinRoom(socket, room) {
   socket.join(room);
   currentRoom[socket.id] = room;
-  socket.emit('joinResult', { room: room });
-  socket.broadcast.to(room).emit('message', {
-    text: nickNames[socket.id] + ' dołączył do pokoju ' + room + '.',
+  socket.emit("joinResult", { room: room });
+  socket.broadcast.to(room).emit("message", {
+    text: nickNames[socket.id] + " dołączył do pokoju " + room + ".",
   });
-  var usersInRoom = io.sockets.clients(room);
+  let usersInRoom = io.sockets.clients(room);
   if (usersInRoom.length > 1) {
-    var usersInRoomSummary = 'Lista użytkowników w pokoju ' + room + ': ';
-    for (var index in usersInRoom) {
-      var userSocketId = usersInRoom[index].id;
-      if (userSocketId != socket.id) {
+    let usersInRoomSummary = "Lista użytkowników w pokoju " + room + ": ";
+    for (const index in usersInRoom) {
+      const userSocketId = usersInRoom[index].id;
+      if (userSocketId !== socket.id) {
         if (index > 0) {
-          usersInRoomSummary += ', ';
+          usersInRoomSummary += ", ";
         }
         usersInRoomSummary += nickNames[userSocketId];
       }
     }
-    usersInRoomSummary += '.';
-    socket.emit('message', { text: usersInRoomSummary });
+    usersInRoomSummary += ".";
+    socket.emit("message", { text: usersInRoomSummary });
   }
 }
 
 function handleNameChangeAttempts(socket, nickNames, namesUsed) {
-  socket.on('nameAttempt', function (name) {
-    if (name.indexOf('Gość') == 0) {
-      socket.emit('nameResult', {
+  socket.on("nameAttempt", function (name) {
+    if (name.indexOf("Gość") === 0) {
+      socket.emit("nameResult", {
         success: false,
         message: 'Nazwa użytkownika nie może rozpoczynać się od słowa "Gość".',
       });
     } else {
-      if (namesUsed.indexOf(name) == -1) {
-        var previousName = nickNames[socket.id];
-        var previousNameIndex = namesUsed.indexOf(previousName);
+      if (namesUsed.indexOf(name) === -1) {
+        const previousName = nickNames[socket.id];
+        const previousNameIndex = namesUsed.indexOf(previousName);
         namesUsed.push(name);
         nickNames[socket.id] = name;
         delete namesUsed[previousNameIndex];
-        socket.emit('nameResult', {
+        socket.emit("nameResult", {
           success: true,
           name: name,
         });
-        socket.broadcast.to(currentRoom[socket.id]).emit('message', {
-          text: previousName + ' zmienił nazwę na ' + name + '.',
+        socket.broadcast.to(currentRoom[socket.id]).emit("message", {
+          text: previousName + " zmienił nazwę na " + name + ".",
         });
       } else {
-        socket.emit('nameResult', {
-          message: 'Ta nazwa jest używana przez innego użytkownika.',
+        socket.emit("nameResult", {
+          message: "Ta nazwa jest używana przez innego użytkownika.",
         });
       }
     }
@@ -90,23 +91,23 @@ function handleNameChangeAttempts(socket, nickNames, namesUsed) {
 }
 
 function handleMessageBroadcasting(socket) {
-  socket.on('message', function (message) {
-    socket.broadcast.to(message.room).emit('message', {
-      text: nickNames[socket.id] + ': ' + message.text,
+  socket.on("message", function (message) {
+    socket.broadcast.to(message.room).emit("message", {
+      text: nickNames[socket.id] + ": " + message.text,
     });
   });
 }
 
 function handleRoomJoining(socket) {
-  socket.on('join', function (room) {
+  socket.on("join", function (room) {
     socket.leave(currentRoom[socket.id]);
     joinRoom(socket, room.newRoom);
   });
 }
 
 function handleClientDisconnection(socket) {
-  socket.on('disconnect', function () {
-    var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+  socket.on("disconnect", function () {
+    const nameIndex = namesUsed.indexOf(nickNames[socket.id]);
     delete namesUsed[nameIndex];
     delete nickNames[socket.id];
   });
