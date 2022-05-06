9ec90b9a68a30f8188493c66a8e584c1bb314ac5
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-05-06 15:34:31 +0200

    Add directory notes

---
+++ exercises/modules/notes/app.js
@@ -0,0 +1,5 @@
+const notes = require('./notes');
+// console.log('Jestem w module app');
+// console.log(notes.txt);
+// const mod = require('module');
+// console.log(mod.wrapper);
---
+++ exercises/modules/notes/notes.js
@@ -0,0 +1,18 @@
+// console.log('Jestem w module notes');
+// exports === module.exports === {}
+
+// console.log('exports', exports);
+// console.log(exports === module.exports);
+
+// console.log('__dirname', __dirname);
+// console.log('__filename', __filename);
+// console.log('require', require);
+
+console.log('module', module);
+
+module.exports = {
+  txt: "zwrócone z notes"
+}
+
+// console.log(exports === module.exports);
+
